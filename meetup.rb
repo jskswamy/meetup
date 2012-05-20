3ebd633e1f42a24c9b46cf831210665aa85a4d47
Krishnaswamy Subramanian <subramk@thoughtworks.com>
2012-05-20 15:35:51 +0530

    Krishna | Styling the topics view

--- meetup.rb
+++ meetup.rb
@@ -5,18 +5,15 @@ require 'yaml'
 require 'rMeetup'
 
 APP_CONFIG = YAML::load(File.read(File.expand_path('config/app.yml','.')))
-
 RMeetup::Client.api_key = APP_CONFIG["meetup_api_key"]
 
 get '/' do
   with_base_layout :index
 end
 
-get '/topics/:search_term' do
-  content_type :json
-  topics = RMeetup::Client.fetch(:topics, {:search => params[:search_term]})
-  result = topics.map { |topic| {:id => topic.id, :name => topic.name, :urlkey => topic.urlkey, :description => topic.description} }
-  result.to_json
+post '/topics' do
+  @topics = RMeetup::Client.fetch(:topics, {:search => params[:search_term], :order => 'name'})
+  with_base_layout :topics
 end
 
 private
